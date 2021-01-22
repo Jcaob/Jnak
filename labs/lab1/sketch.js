0c2104bec96afcf989e61fd77963952163196fdd
Jcaob <jcaobcatalan943@gmail.com>
2021-01-22 15:50:36 -0500

    Perfect compisition

---
+++ labs/lab1/sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  noStroke();
+  let a = color(255, 88, 110, 102);
+  let b = alpha(a);
+  fill(b);
+  background(220);
+  fill(a);
+  rect(135, 80, 30, 10, );
+  rect(185, 80, 30, 10 );
+  circle(150, 100,10);
+  circle(200, 100, 10);
+  square(160, 110, 25,10);
+  arc(160, 140, 50, 50, 0, PI + QUARTER_PI);
+  ellipse(56, 46, 55, 55);
+}
