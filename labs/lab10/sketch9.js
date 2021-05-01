9a902d92c90d74a39d7197d17a4923e92a418232
Jcaob <jcaobcatalan943@gmail.com>
2021-05-01 19:08:09 -0400

    final

--- labs/lab10/sketch9.js
+++ labs/lab10/sketch9.js
@@ -3,9 +3,7 @@
  colors = {"squareOne": "#eb4034", "squareTwo": "#34eb6b", "squareThree": "#345ceb"}
 // this is the oblject of colors that the divs take from
 
-
-
- function changeColor(e) {
+function changeColor(e) {
    var target = e.target || e.srcElement;
     target.style.backgroundColor = colors[target.id];
     // this goes through each of the divs with the target and gets the color
