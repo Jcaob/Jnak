baf1b6fc2c13dad11fdaf643e7de1b6c6822a6f2
Jcaob <jcaobcatalan943@gmail.com>
2021-07-17 20:21:10 -0400

    e

---
+++ labs/lab4/sketch3.js
@@ -0,0 +1,15 @@
+let Div = document.getElementsByClassName("Yep")
+
+
+
+for(let i = 0; i < Div.length; i++){
+    TweenLite.from(Div[i],
+        {duration: .3, x: -100, alpha: 0, delay: i }
+        );
+}
+
+function click(DivNum)
+{
+    TweenLite.to(Div[DivNum], {duration:1, alpha:0})
+}
+    
